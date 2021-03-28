2fb67cf542caf5288fb19f25690fd6b6713eb8a7
JGreenOS <jsciteach@gmail.com>
2021-03-28 14:22:09 -0400

    building packages and files

---
+++ index.js
@@ -0,0 +1,13 @@
+//inquirer package
+//add department
+//add roles
+//add employees
+//update employee managers
+//view employees by manager
+//delete department
+//delete role
+//delete employee 
+
+
+//what to do based on choices (switch statements)
+
