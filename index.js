3309f315e98e99049ff8e175a1a8577e354e7a55
JGreenOS <jsciteach@gmail.com>
2021-03-29 23:49:46 -0400

    adding prompts and ascii art

--- index.js
+++ index.js
@@ -1,6 +1,91 @@
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+const { exit } = require("node:process");
+require ("console.table");
+
+init ();
+
+function init() {
+const logoText = logo ( { name: "Employee Management System"}).render();
+console.log(logoText);
+
+loadMenu();
+}
+
 //inquirer package
-//add department
-//add roles
+function loadMenu () {
+    const { choice } await prompt([
+        {
+            type: "list",
+            name: "choice",
+            messsage: "Select an action",
+            choices: [
+               {
+                   name: "View All Departments",
+                   value: "VIEW_DEPTS"
+               },
+               {
+                   name: "View All Roles",
+                   value: "VIEW_ROLES"
+               },
+
+
+{
+name: "View All Employees",
+value: "VIEW_ALLEMPLOYEES"
+
+},
+
+                {
+
+        
+                    name: "Add Department",
+                    value: "ADD_DEPT"
+                },
+                {
+                    name: "Remove Department",
+                    value: "DELETE_DEPT"
+
+                },
+
+                {
+                    name: "Add Roles",
+                    value: "ADD_ROLES"
+                },
+
+                {
+                    name: "Delete Roles - be careful this can make a mess",
+                    value: "DELETE_ROLES"
+                },
+                {
+                name: "Add Employee",
+                value: "ADD_EMPLOYEE"
+                }, 
+                {
+                name: "Remove Employee",
+                value: "DELETE_EMPLOYEE"
+            },
+            {
+                name: ""
+            }
+            {
+                name: "Update Employee Role",
+                value: "UPDATE_ROLE"
+            },
+            {
+                name: "Exit system",
+                value: "EXIT"
+            },
+
+            //close out inquirer here
+
+
+
+
+
+
+
 //add employees
 //update employee managers
 //view employees by manager
